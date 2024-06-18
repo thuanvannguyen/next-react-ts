16fc7a3f7ab5053f16dab69a7f2f1a3119e0765c
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-06-19 00:56:01 +0700

    Add app logic

---
+++ ts/app.ts
@@ -0,0 +1,54 @@
+//// Ex-1
+let variable = "hello";
+variable = "hi";
+
+let age = 18;
+// age = "123";
+
+//// Ex-2
+let ageWithType: number = 22;
+
+// ageWithType ="eighteen"
+ageWithType = 18;
+
+//// Ex-3
+let testString: string;
+
+testString = "hello";
+
+let testBoolean: boolean;
+
+testBoolean = false;
+
+//// Ex-4
+let testStringOrNumber: string | number;
+
+testStringOrNumber = 10;
+testStringOrNumber = "10";
+// testStringOrNumber = []
+
+//// Ex-5
+let names = ["john", "jane", "tom"];
+
+// names.push(3)
+names.push("mike");
+
+let numbers = [11, 22, 35];
+
+// numbers.push(true)
+numbers.push(92);
+
+let testStringArray: string[];
+
+// testStringArray = [1,2,3]
+testStringArray = ["one", "two", "three"];
+
+let testNumberArray: number[];
+
+// testNumberArray = [true, "hi", 23]
+testNumberArray = [12, 55, 23];
+
+let testStringOrNumberArray: (string | number)[];
+
+testStringOrNumberArray = [1, "two", 3];
+
